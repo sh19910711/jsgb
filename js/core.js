2626361758e9ae3a76ad7f53cd3a3bb8883426a1
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 07:12:21 +0000

    core: add memory map as comment

--- js/core.js
+++ js/core.js
@@ -323,6 +323,32 @@ class Operations {
   }
 }
 
+/*********************************
+Interrupt Enable Register
+-------------------------- FFFF
+Internal RAM
+-------------------------- FF80
+Empty but unusable for I/O
+-------------------------- FF4C
+I/O ports
+-------------------------- FF00
+Empty but unusable for I/O
+-------------------------- FEA0
+Sprite Attrib Memory
+-------------------------- FE00
+Echo of 8kB Internal RAM
+-------------------------- E000
+8kB Internal RAM
+-------------------------- C000
+8kB switchable RAM bank
+-------------------------- A000
+8kB Video RAM
+-------------------------- 8000 --
+16kB switchable ROM bank
+-------------------------- 4000   = 32kB Cartridge
+16kB ROM bank #0
+-------------------------- 0000 --
+**********************************/
 class RAM {
   constructor() {
     this.data = new Array(0x10000);
