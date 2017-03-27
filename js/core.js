43ee3b57b039a06a6990398b02a049a2cfab998e
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 15:28:02 +0000

    core: add RRr_B

--- js/core.js
+++ js/core.js
@@ -2228,6 +2228,16 @@ class Operations {
     this.r.FC = r & 0x01;
     this.cycles = 8;
   }
+
+  RRr_B() {
+    const r = this.r.B;
+    const v = (this.r.B = (r >> 1) & 0xFF | this.r.FC << 7);
+    this.r.FZ = v == 0;
+    this.r.FN = 0;
+    this.r.FH = 0;
+    this.r.FC = r & 0x01;
+    this.cycles = 8;
+  }
 }
 
 function zero8(d0, d1, d2) {
