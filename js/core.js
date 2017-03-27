b3f280a61e03b1b8d9eb77fa0f2f83d51a2f8426
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 12:51:47 +0000

    core: add ADCAHL

--- js/core.js
+++ js/core.js
@@ -932,6 +932,18 @@ class Operations {
     this.r.FC = carry8(d0, d1, d2);
     this.cycles = 8;
   }
+
+  ADCAHL() {
+    const d0 = this.r.A;
+    const d1 = this.m.read8(this.r.HL);
+    const d2 = this.r.FC;
+    this.r.A = d0 + d1 + d2;
+    this.r.FZ = zero8(d0, d1, d2);
+    this.r.FN = 0;
+    this.r.FH = halfCarry8(d0, d1, d2);
+    this.r.FC = carry8(d0, d1, d2);
+    this.cycles = 8;
+  }
 }
 
 function zero8(d0, d1, d2) {
