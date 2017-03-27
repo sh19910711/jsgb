8eec340c7038477000c6019c9f93d4871716ef1a
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 13:34:18 +0000

    core: add XORr_D

--- js/core.js
+++ js/core.js
@@ -1387,6 +1387,17 @@ class Operations {
     this.r.FC = 0;
     this.cycles = 4;
   }
+
+  XORr_D() {
+    const d0 = this.r.A;
+    const d1 = this.r.D;
+    this.r.A = d0 ^ d1;
+    this.r.FZ = (d0 ^ d1) == 0;
+    this.r.FN = 0;
+    this.r.FH = 0;
+    this.r.FC = 0;
+    this.cycles = 4;
+  }
 }
 
 function zero8(d0, d1, d2) {
