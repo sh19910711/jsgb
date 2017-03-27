efa82ac9fa5eae870fb666a3b375429f8fd195c3
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 17:11:03 +0000

    core: add JPccnn_C

--- js/core.js
+++ js/core.js
@@ -2694,6 +2694,17 @@ class Operations {
       this.cycles = 12;
     }
   }
+
+  JPccnn_C() {
+    const addr = this.m.read16(this.r.PC);
+    this.tickn(2);
+    if (this.r.FC) {
+      this.r.PC = addr;
+      this.cycles = 16;
+    } else {
+      this.cycles = 12;
+    }
+  }
 }
 
 function zero8(d0, d1, d2) {
