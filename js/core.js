8cd032e8646b1758bfb53e2988d0747f949317e9
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 13:24:25 +0000

    core: add SBCr_B

--- js/core.js
+++ js/core.js
@@ -1056,6 +1056,18 @@ class Operations {
     this.r.FC = carrySub8(d0, d1, d2);
     this.cycles = 4;
   }
+
+  SBCr_B() {
+    const d0 = this.r.A;
+    const d1 = this.r.B;
+    const d2 = this.r.FC;
+    this.r.A = d0 - d1 - d2;
+    this.r.FZ = zeroSub8(d0, d1, d2);
+    this.r.FN = 1;
+    this.r.FH = halfCarrySub8(d0, d1, d2);
+    this.r.FC = carrySub8(d0, d1, d2);
+    this.cycles = 4;
+  }
 }
 
 function zero8(d0, d1, d2) {
