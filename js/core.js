fc8c48387910ab266b9868f62f1d6b0cd9b03527
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 14:06:10 +0000

    core: add INCHL

--- js/core.js
+++ js/core.js
@@ -1617,6 +1617,17 @@ class Operations {
     this.r.FC = carry8(r, 1);
     this.cycles = 4;
   }
+
+  INCHL() {
+    const d = this.m.read8(this.r.HL);
+    const v = d + 1;
+    this.m.write8(this.r.HL, v);
+    this.r.FZ = v == 0;
+    this.r.FN = 0;
+    this.r.FH = halfCarry8(r, 1);
+    this.r.FC = carry8(r, 1);
+    this.cycles = 12;
+  }
 }
 
 function zero8(d0, d1, d2) {
