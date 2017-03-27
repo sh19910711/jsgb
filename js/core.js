09ba0afab699e3269eb05136ad26d31598c90714
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 16:40:03 +0000

    core: add SRLr_D

--- js/core.js
+++ js/core.js
@@ -2482,6 +2482,16 @@ class Operations {
     this.r.FC = r & 0x01;
     this.cycles = 8;
   }
+
+  SRLr_D() {
+    const r = this.r.D;
+    const v = (this.r.D = r >> 1 & 0x7F);
+    this.r.FZ = v == 0;
+    this.r.FN = 0;
+    this.r.FH = 0;
+    this.r.FC = r & 0x01;
+    this.cycles = 8;
+  }
 }
 
 function zero8(d0, d1, d2) {
