526fb529fb1e14f7efdcc14e3e8972a49daa9757
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 16:36:36 +0000

    core: add SLAr_A

--- js/core.js
+++ js/core.js
@@ -2299,6 +2299,17 @@ class Operations {
     this.r.FC = r & 0x01;
     this.cycles = 16;
   }
+
+  // Shifts
+  SLAr_A() {
+    const r = this.r.A;
+    const v = (this.r.A = (r << 1) & 0xFE);
+    this.r.FZ = v == 0;
+    this.r.FN = 0;
+    this.r.FH = 0;
+    this.r.FC = r & 0x80;
+    this.cycles = 8;
+  }
 }
 
 function zero8(d0, d1, d2) {
