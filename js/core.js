1bcd653b0825ed9e94a49c48ae59b656e22c06b1
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 14:12:51 +0000

    core: add DECr_L

--- js/core.js
+++ js/core.js
@@ -1688,6 +1688,16 @@ class Operations {
     this.r.FC = 0;
     this.cycles = 4;
   }
+
+  DECr_L() {
+    const r = this.r.L;
+    const v = (this.r.L = r - 1);
+    this.r.FZ = v == 0;
+    this.r.FN = 1;
+    this.r.FH = halfCarrySub8(r, 1);
+    this.r.FC = 0;
+    this.cycles = 4;
+  }
 }
 
 function zero8(d0, d1, d2) {
