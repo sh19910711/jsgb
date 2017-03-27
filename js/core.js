de5853af1a59c94b1db074bac3a0b344e8112ee4
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 14:06:02 +0000

    core: add INCr_L

--- js/core.js
+++ js/core.js
@@ -1607,6 +1607,16 @@ class Operations {
     this.r.FC = carry8(r, 1);
     this.cycles = 4;
   }
+
+  INCr_L() {
+    const r = this.r.L;
+    const v = (this.r.L = r + 1);
+    this.r.FZ = v == 0;
+    this.r.FN = 0;
+    this.r.FH = halfCarry8(r, 1);
+    this.r.FC = carry8(r, 1);
+    this.cycles = 4;
+  }
 }
 
 function zero8(d0, d1, d2) {
