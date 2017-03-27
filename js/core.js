7db1d999b0a73c6197ddd79ab3bf5427bd6e1a7e
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 14:35:20 +0000

    core: add SWAPr_H

--- js/core.js
+++ js/core.js
@@ -1837,6 +1837,16 @@ class Operations {
     this.r.FC = 0;
     this.cycles = 8;
   }
+
+  SWAPr_H() {
+    const v = this.r.H;
+    const s = (this.r.H = (v & 0xF0) >> 4 | (v & 0x0F) << 4);
+    this.r.FZ = s == 0;
+    this.r.FN = 0;
+    this.r.FH = 0;
+    this.r.FC = 0;
+    this.cycles = 8;
+  }
 }
 
 function zero8(d0, d1, d2) {
