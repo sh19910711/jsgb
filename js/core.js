064be13deca3158e1dc9c7a1dca865c0f606e3a8
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 16:56:01 +0000

    core: add BITr_C

--- js/core.js
+++ js/core.js
@@ -2572,6 +2572,13 @@ class Operations {
     this.r.FH = 1;
     this.cycles = 8;
   }
+
+  BITr_C(b) {
+    this.r.FZ = !(this.r.C & 1 << b);
+    this.r.FN = 0;
+    this.r.FH = 1;
+    this.cycles = 8;
+  }
 }
 
 function zero8(d0, d1, d2) {
