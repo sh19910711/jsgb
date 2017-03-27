a62c48282f9deeb8b005c1e08d9d90e6937fdace
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 15:26:54 +0000

    core: add RRCr_D

--- js/core.js
+++ js/core.js
@@ -2157,6 +2157,16 @@ class Operations {
     this.r.FC = r & 0x01;
     this.cycles = 8;
   }
+
+  RRCr_D() {
+    const r = this.r.D;
+    const v = (this.r.D = (r >> 1) & 0xFF | (r & 0x01) << 7);
+    this.r.FZ = v == 0;
+    this.r.FN = 0;
+    this.r.FH = 0;
+    this.r.FC = r & 0x01;
+    this.cycles = 8;
+  }
 }
 
 function zero8(d0, d1, d2) {
