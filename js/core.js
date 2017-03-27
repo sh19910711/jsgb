97c422389a4b77210134bf0d7eed520ca7625394
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 16:39:32 +0000

    core: add SRAHL

--- js/core.js
+++ js/core.js
@@ -2451,6 +2451,17 @@ class Operations {
     this.r.FC = r & 0x01;
     this.cycles = 8;
   }
+
+  SRAHL() {
+    const r = this.m.read8(this.r.HL);
+    const v = r & 0x80 | r >> 1;
+    this.m.write8(this.r.HL, v);
+    this.r.FZ = v == 0;
+    this.r.FN = 0;
+    this.r.FH = 0;
+    this.r.FC = r & 0x01;
+    this.cycles = 16;
+  }
 }
 
 function zero8(d0, d1, d2) {
