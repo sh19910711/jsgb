6c9f91015750a818375a3bb8f649274d6c71cb71
Hiroyuki Sano <sh19910711@gmail.com>
2017-03-27 01:35:26 +0000

    core: add registers

--- js/core.js
+++ js/core.js
@@ -1,6 +1,40 @@
+class Registers {
+  constructor() {
+    this._ = {
+      A: 0,
+      B: 0,
+      D: 0,
+      H: 0,
+      FZ: 0,
+      FN: 0,
+      FH: 0,
+      FC: 0,
+      C: 0,
+      E: 0,
+      L: 0,
+      SP: 0,
+      PC: 0,
+    };
+  }
+
+  get A() { return this._.A & 0xFF }
+  get B() { return this._.B & 0xFF }
+  get D() { return this._.D & 0xFF }
+  get H() { return this._.H & 0xFF }
+  get FZ() { return this._.FZ & 0xFF }
+  get FN() { return this._.FN & 0xFF }
+  get FH() { return this._.FH & 0xFF }
+  get FC() { return this._.FC & 0xFF }
+  get C() { return this._.C & 0xFF }
+  get E() { return this._.E & 0xFF }
+  get L() { return this._.L & 0xFF }
+  get SP() { return this._.SP & 0xFF }
+  get PC() { return this._.PC & 0xFF }
+}
+
 class Core {
   constructor() {
-    this.clk = 0;
+    this.reg = new Registers;
   }
 
   tick() {
